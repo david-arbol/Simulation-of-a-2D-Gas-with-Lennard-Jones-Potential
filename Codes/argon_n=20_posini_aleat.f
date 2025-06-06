4cf839fbcb28aee7b22e7a68619d5afd5377e2d4
David Árbol <147392042+davidjarbol@users.noreply.github.com>
2025-06-06 11:15:53 -0400

    Update and rename argon_n=20_posini_aleat.f to Codes/argon_n=20_posini_aleat.f

--- Codes/argon_n=20_posini_aleat.f
+++ Codes/argon_n=20_posini_aleat.f
@@ -12,7 +12,7 @@
       integer i,j,n,p,q
       real*8 pdec
       real*8 paux(2)
-      !Variables para c lculos postenriores
+      !Variables para cî¹‡culos postenriores
       real*8 Ec,Ep,Et,Temp
       integer cont
       real*8, dimension(:,:), allocatable :: radio
@@ -40,7 +40,7 @@
 
       
       d=10.d0 !Dimensiones (dxd) de mi cristal bidimensional
-              !Debe ser un n£mero natural
+              !Debe ser un nÏƒero natural
               
 
       !Abro archivos
@@ -59,7 +59,7 @@
       !Inicializo a cero la aceleraciones para luego sumarlas
       do i=1,n
          do j=1,n
-          acelx(i,j)=0.d0  !Aceleraci¢n de i ejercida por j
+          acelx(i,j)=0.d0  !Aceleraciâ–‹ de i ejercida por j
           acely(i,j)=0.d0
           radio(i,j)=0.d0
          enddo
@@ -102,7 +102,7 @@
       write(2,*)
       write(2,*)
 
-      !Inicializo las velocidades iniciales a modulo 1 y  ngulo random
+      !Inicializo las velocidades iniciales a modulo 1 y î¹‰gulo random
 
       do i=1,n
            ang_aux=dran_u()*4.d0*datan(1.d0)*2.d0
@@ -145,7 +145,7 @@
             radio(j,i)=radio(i,j)
          enddo
       enddo
-      !Aceleraci¢n total de cada part¡cula
+      !Aceleraciâ–‹ total de cada partï¸·ula
       do i=1,n
         do j=1,n
            a1(i,1)=a1(i,1)+acelx(i,j)
@@ -154,12 +154,12 @@
       enddo
 
       
-      !ENERGÖAS:
+      !ENERGæ·²S:
                 !Inicializo
       Ec=0.d0
       Ep=0.d0
       Et=0.d0
-                !Energ¡a cin‚tica:
+                !Energï½› cinî¾‰ica:
       do i=1,n
          Ec=Ec+0.5d0*(v(i,1)**2.d0+v(i,2)**2.d0)
          do j=i+1,n
@@ -170,13 +170,13 @@
       write(5,*) t, Ec, Ep, Et
 
 
-!!!!!!!ALGORITMO PARA CµLCULO MECµNICA  -------------------------------
+!!!!!!!ALGORITMO PARA Cç„¡CULO MECç…®ICA  -------------------------------
       do p=1,90000
         !Calculo omega
         omega=v+dt*0.5d0*a1
-        !Calculo posici¢n siguiente
+        !Calculo posiciâ–‹ siguiente
         r_aux=r+dt*omega
-        !Cond peri¢dicas en posici¢n
+        !Cond periâ–ƒicas en posiciâ–‹
         do i=1,n
           do j=1,2
           pdec=(r_aux(i,j)/d)-dfloat(int(r_aux(i,j)/d))
@@ -190,7 +190,7 @@
           enddo
         enddo
 
-!!!!!!!Calculo aceleraci¢n siguiente
+!!!!!!!Calculo aceleraciâ–‹ siguiente
       do i=1,n
         do j=1,n
            acelx(i,j)=0.d0
@@ -226,7 +226,7 @@
             radio(j,i)=radio(i,j)
          enddo
       enddo
-      !Aceleraci¢n total de cada part¡cula
+      !Aceleraciâ–‹ total de cada partï¸·ula
       do i=1,n
         do q=1,2
            a1(i,q)=0.d0
@@ -242,7 +242,7 @@
         !Calculo velocidad siguiente
         v=omega+dt*0.5d0*a1
 
-        !Calculo todo lo demÂ s
+        !Calculo todo lo demï¿½s
         t=t+dt
 
         !Escribo posiciones en fichero
@@ -254,11 +254,11 @@
 
         write(4,*) ((a1(i,j), j=1,2), i=1,n)
 
-        !Energ¡as en cada paso
+        !Energï½›s en cada paso
         Ec=0.d0
         Ep=0.d0
         Et=0.d0
-                !Energ¡a cin‚tica:
+                !Energï½› cinî¾‰ica:
       do i=1,n
          Ec=Ec+0.5d0*(v(i,1)**2.d0+v(i,2)**2.d0)
          do j=i+1,n
